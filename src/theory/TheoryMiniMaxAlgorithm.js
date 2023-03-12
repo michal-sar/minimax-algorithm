a2978641ae9593d06c3b6c1425f50cb6569c059b
Michal Sar <michal_sar@onet.pl>
2023-03-12 16:02:52 +0100

    add images to minimax algorithm section

--- src/theory/TheoryMiniMaxAlgorithm.js
+++ src/theory/TheoryMiniMaxAlgorithm.js
@@ -1,5 +1,8 @@
 import React from "react";
 import SimpleBar from "simplebar-react";
+import GameTree from "../../public/images/game-tree.svg";
+import DepthFirst from "../../public/images/depth-first.svg";
+import MiniMax from "../../public/images/minimax.svg";
 
 function TheoryMiniMaxAlgorithm() {
   return (
@@ -24,17 +27,32 @@ function TheoryMiniMaxAlgorithm() {
         Most games can be represented in an abstract way by a <u>game tree</u>.
         The nodes of a game tree represent the specific states of the game, and
         the edges represent moves made by the players. The root node is the{" "}
-        <u>initial state</u>, and the leaf nodes are the <u>final states</u> of
-        the game – states where the outcome is known.
+        <span className="outlineGreen">initial state</span>, and the leaf nodes
+        are the <span className="outlineRed">final states</span> of the game –
+        states where the outcome is known.
       </p>
-      [Image]
       <p>
         To determine the optimal move in the given situation, the MiniMax
         algorithm performs a <u>depth-first traversal</u> of the{" "}
         <u>partial game tree</u> starting at the node representing the{" "}
-        <u>current state</u> of the game. It evaluates every node it encounters.
+        <span className="outlineYellow">current state</span> of the game. It
+        evaluates every node it encounters.
       </p>
-      [Image]
+      <div className="imageContainer">
+        <div>
+          <GameTree />
+          <p className="imageDescription">
+            <b>Image 1:</b> <u>Game tree</u> representing tic-tac-toe.
+          </p>
+        </div>
+        <div>
+          <DepthFirst />
+          <p className="imageDescription">
+            <b>Image 2:</b> <u>Depth-first traversal</u> of{" "}
+            <u>partial game tree</u> representing tic-tac-toe.
+          </p>
+        </div>
+      </div>
       <p>
         If the evaluated state is final, a <u>utility function</u> (also called
         a <u>payoff function</u>) assigns a score to the state based on the
@@ -77,10 +95,8 @@ function TheoryMiniMaxAlgorithm() {
         f — final state
         <br />
         F — set of all final states (f ∈ F, F ⊆ N)
-        <br />
-        U — utility function
-        <br />
-        S — successor function
+        <br />U — <u>utility function</u>
+        <br />S — <u>successor function</u>
         <br />
         Max — returns the highest number given as an input parameter
         <br />
@@ -146,15 +162,22 @@ function TheoryMiniMaxAlgorithm() {
         .
       </p>
       <p>Below you can see the MiniMax algorithm in action:</p>
-      [Image]
+      <div className="imageContainer">
+        <div>
+          <MiniMax />
+          <p className="imageDescription">
+            <b>Image 3:</b> MiniMax algorithm in action.
+          </p>
+        </div>
+      </div>
       <p>
         The example above shows a certain redundancy in the MiniMax algorithm.
-        After a score of 1 has been assigned to the middle branch, these
-        evaluations of the right branch are unnecessary. The maximizer will
-        choose the middle branch (<span className="monospaced">score = 1</span>)
-        over the right branch (
+        After a score of 0 has been assigned to the left branch,{" "}
+        <span className="outlineBlue">these evaluations</span> of the other
+        branches are unnecessary. The maximizer will choose the left branch (
+        <span className="monospaced">score = 0</span>) over the other branches (
         <span className="monospaced">score = Min(-1, x) ≤ -1</span>) because{" "}
-        <span className="monospaced">Max(1, Min(-1, x)) = 1</span> no matter
+        <span className="monospaced">Max(0, Min(-1, x)) = 0</span> no matter
         what the value of <span className="monospaced">x</span> is.
       </p>
       <p>
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
